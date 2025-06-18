4c0303c0db9e00f585b7a2b58c8d9020b4ee0a5d
Ole Kodehode <ole.kodehode@gmail.com>
2025-06-18 15:45:18 +0200

    Initial state + logic

---
+++ dom-manipulation.js
@@ -0,0 +1,22 @@
+function createAndAppendElement(parent, tag, attributes, children) {
+  const element = document.createElement(tag);
+  for (const [key, value] of Object.entries(attributes)) {
+    element.setAttribute(key, value);
+  }
+  for (const child of children) {
+    if (typeof child === "string") {
+      element.appendChild(document.createTextNode(child));
+    } else {
+      element.appendChild(child);
+    }
+  }
+  parent.appendChild(element);
+  return element;
+}
+
+function clearBodyElement() {
+  const body = document.body;
+  while (body.firstChild) {
+    body.removeChild(body.firstChild);
+  }
+}
---
+++ todo.js
@@ -0,0 +1,35 @@
+/**
+ * @typedef {Object} Todo
+ * @property {number} userId
+ * @property {number} id
+ * @property {string} title
+ * @property {boolean} completed
+ */
+
+class TodoList {
+  constructor() {
+    /** @type {Array<Todo> | null} */
+    this.todos = null;
+  }
+
+  /**
+   * Fetch todos from the API
+   * @returns {Promise<Array<Todo>>} A promise that resolves to an array of todos
+   */
+  fetchTodos() {
+    return new Promise((resolve, reject) => {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => response.json())
+        .then((/** @type {Array<Todo>} */ data) => {
+          this.todos = data;
+          resolve(data);
+        })
+        .catch((error) => {
+          this.todos = null;
+          reject(error);
+        });
+    });
+  }
+}
+
+const todoList = new TodoList();
