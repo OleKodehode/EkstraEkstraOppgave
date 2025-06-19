70889809663be11e4708e371043db8f902b27e00
Ole Kodehode <ole.kodehode@gmail.com>
2025-06-19 12:43:25 +0200

    Removed task ID section of form, added filtering for User ID and clearing the filter

---
+++ filtering.js
@@ -0,0 +1,71 @@
+function filterResult() {
+  const userid_value = document.getElementById("userid_box").value;
+
+  populateArticles(userid_value);
+}
+
+function clearFilter() {
+  const userid_box = document.getElementById("userid_box");
+
+  userid_box.value = "";
+  populateArticles();
+}
+
+function populateArticles(userId = undefined) {
+  // clear main container of items
+  const main_container = document.getElementById("main_container");
+  while (main_container.firstChild) {
+    main_container.removeChild(main_container.firstChild);
+  }
+
+  todoList
+    .fetchTodos(userId ? userId : undefined)
+    .then((todos) => {
+      for (const todo of todos) {
+        // ... Add the todo-entry to the DOM here!
+        // Create Checkbox - Add checked if task is completed
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        if (todo.completed) {
+          checkbox.setAttribute("checked", true);
+        }
+
+        // Create the paragraph for the task text
+        const task_text = document.createElement("p");
+        task_text.innerText = todo.title;
+
+        // Use the utility script to create an article container for the todo item
+        // and append it to main
+        const todo_article = createAndAppendElement(
+          main_container,
+          "article",
+          {
+            class: `userID-${todo.userId}`,
+            id: `taskID-${todo.id}`,
+          },
+          [checkbox, task_text]
+        );
+      }
+    })
+    // If there is an error, we log it and set the todos to null
+    .catch((error) => {
+      console.error("Error fetching todos:", error);
+      todoList.todos = null;
+    })
+    .finally(() => {
+      // If the todos are null, we log a message
+      if (todoList.todos === null) {
+        console.log("No todos found");
+        // Add some user-friendly feedback to the DOM.
+        const feedbackElement = createAndAppendElement(
+          document.body,
+          "p",
+          {
+            class: "feedback",
+            id: "feedback",
+          },
+          ["No todos found"]
+        );
+      }
+    });
+}
--- todo.js
+++ todo.js
@@ -16,12 +16,20 @@ class TodoList {
    * Fetch todos from the API
    * @returns {Promise<Array<Todo>>} A promise that resolves to an array of todos
    */
-  fetchTodos() {
+  fetchTodos(filter = undefined) {
     return new Promise((resolve, reject) => {
       fetch("https://jsonplaceholder.typicode.com/todos")
         .then((response) => response.json())
         .then((/** @type {Array<Todo>} */ data) => {
           this.todos = data;
+
+          if (filter !== undefined) {
+            const filterData = data.filter((e) => e.userId == filter);
+            console.log(filterData);
+            resolve(filterData);
+            return;
+          }
+
           resolve(data);
         })
         .catch((error) => {
